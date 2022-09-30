9ae26dbe09e4bdfbc05943711e5b03f799c59e5f
Shinglington <4kshing@gmail.com>
2022-09-30 11:17:36 +0100

    added raw_input func

--- Caesar.py
+++ Caesar.py
@@ -1,5 +1,13 @@
-import sys
 
-text = sys.stdin.read()
-print(text)
-input()
+def raw_input(prompt):
+    lines = []
+    print(prompt)
+    while True:
+        line = input()
+        if line:
+            lines.append(line)
+        else:
+            break
+    return ' '.join(lines)
+
+
