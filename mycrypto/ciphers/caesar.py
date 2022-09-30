b8c3e08577d0c28adcd8784a5a5c7b6ec71a0275
Shinglington <4kshing@gmail.com>
2022-09-30 11:43:30 +0100

    Created encrypt function

--- mycrypto/ciphers/caesar.py
+++ mycrypto/ciphers/caesar.py
@@ -1,1 +1,14 @@
+import string
 
+def encrypt(plaintext, shift, alphabet = string.ascii_lowercase):
+    ciphertext = ""
+    for c in plaintext:
+        new_char = c.lower()
+        if (new_char) in alphabet:
+            new_char = alphabet[(alphabet.index(new_char) + shift) % len(alphabet)]
+            if c.isUpper():
+                new_char = new_char.upper()
+        ciphertext = ciphertext + new_char
+    return ciphertext
+    
+def decrypt(plaintext, shift = None, alphabet = string.ascii_lowercase):
