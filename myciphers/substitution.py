712e7da1e30436a6fef4004ec642af22e5781059
Shinglington <4kshing@gmail.com>
2022-10-01 19:01:23 +0100

    Added skeleton of substitution cipher

--- myciphers/substitution.py
+++ myciphers/substitution.py
@@ -1,1 +1,12 @@
-def encrypt()
+from curses import keyname
+from cipher import Cipher
+
+class substition(Cipher):
+    def __init__(self, key = "abcdefghijklmnopqrstuvwxyz", alphabet = "abcdefghijklmnopqrstuvwxyz", keep_case = False):
+        Cipher.__init__(self, key, alphabet, keep_case)
+
+    def encrypt(self, text, keep_spaces = False, keep_punct = False, keep_num = False):
+        return text
+
+    def decrypt(self, text, keep_spaces=False, keep_punct=False, keep_num=False):
+        return text
