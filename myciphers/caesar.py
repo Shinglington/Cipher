f049432ccfe92d0f06d1eea2cde5c85c5b6a6f21
Shinglington <4kshing@gmail.com>
2022-10-01 18:16:16 +0100

    Updated README to add credits

--- myciphers/caesar.py
+++ myciphers/caesar.py
@@ -1,22 +1,25 @@
-def encrypt(plaintext, shift, alphabet = "abcdefghijklmnopqrstuvwxyz"):
-    ciphertext = ""
-    for c in plaintext:
-        new_char = c.lower()
-        if (new_char) in alphabet:
-            new_char = alphabet[(alphabet.index(new_char) + shift) % len(alphabet)]
-            if c.isupper():
-                new_char = new_char.upper()
-        ciphertext = ciphertext + new_char
-    return ciphertext
+from cipher import Cipher
 
-def decrypt(ciphertext, shift = None, alphabet = "abcdefghijklmnopqrstuvwxyz"):
-    plaintext = ""
-    if shift != None:
-        plaintext = encrypt(ciphertext, -shift, alphabet)
-    else:
-        deciphered = []
-        for i in range(len(alphabet)):
-            deciphered.append(decrypt(ciphertext, i, alphabet))
-        plaintext = '\n\n\n'.join(deciphered)
+class Caesar(Cipher):
+    def __init__(self, key = 13, alphabet = "abcdefghijklmnopqrstuvwxyz", keep_punct = False):
+        self.alphabet = alphabet
+        self.key = key % len(alphabet)
+    
+    def encrypt(self, text):
+        if not keep_punct:
+            
+        ciphertext = ""
+        for c in text:
+            new_char = c.lower()
+            if (new_char) in self.alphabet:
+                new_char = self.alphabet[(self.alphabet.index(new_char) + self.key) % len(self.key)]
+                if c.isupper():
+                    new_char = new_char.upper()
+            ciphertext = ciphertext + new_char
+        return ciphertext
 
-    return plaintext
+    def decrypt(text):
+            plaintext = encrypt(ciphertext, -shift, alphabet)
+
+
+        return 
