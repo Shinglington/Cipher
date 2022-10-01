13a74d1542ce61f6496e70e171286211ab4e5fd2
Shinglington <4kshing@gmail.com>
2022-10-01 18:34:17 +0100

    Added prep_text method. Removed remove_punct

--- myciphers/cipher.py
+++ myciphers/cipher.py
@@ -1,8 +1,27 @@
 
 class Cipher():
+    def __init__(self, key, alphabet, ignore_case = True):
+        self.key = key
+        self.alphabet = alphabet
+        self.ignore_case = ignore_case
+
     def encrypt(self, text):
         return text
     
     def decrypt(self, text):
         return text
     
+    def prep_text(self, text, keep_spaces = False, keep_punct = False, keep_num = False):
+        output = ""
+        filter = ""
+        if not keep_spaces:
+            filter += ' '
+        if not keep_punct:
+            filter += "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+        if not keep_num:
+            filter += "0123456789"
+
+        for c in text:
+            if c not in filter:
+                output += c
+        return output
