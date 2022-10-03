f6bb19e052b0da30e0754ffab5c38e9be40fa386
Shinglington <4kshing@gmail.com>
2022-10-03 22:12:10 +0100

    Created Caesar_interface

--- console_interface.py
+++ console_interface.py
@@ -1,18 +1,8 @@
 import myciphers
 
-def raw_input(prompt):
-    lines = []
-    print(prompt)
-    while True:
-        line = input()
-        if line:
-            lines.append(line)
-        else:
-            break
-    return ' '.join(lines)
 
 
 def main():
-    text = raw_input("Enter cipher text:")
+    text = myciphers.raw_input("Enter cipher text:")
  
 main()
--- myciphers/utility.py
+++ myciphers/utility.py
@@ -1,5 +1,15 @@
 import string
 
+def raw_input(prompt):
+    lines = []
+    print(prompt)
+    while True:
+        line = input()
+        if line:
+            lines.append(line)
+        else:
+            break
+    return ' '.join(lines)
 
 def ngram(text, n=1, ignore_case = True, continuous = True, display = True):
     frequencies = {}
