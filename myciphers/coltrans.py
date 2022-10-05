7a96196f70fbbdeac3ef7b70f7381aff1fb09769
Shinglington <4kshing@gmail.com>
2022-10-05 09:24:49 +0100

    Added make_columns method

--- myciphers/coltrans.py
+++ myciphers/coltrans.py
@@ -2,14 +2,28 @@ from myciphers.cipher import TransCipher
 
 class ColTrans(TransCipher):
     def __init__(self, key = "ABCDEF"):
-        Cipher.__init__(self, key.lower(), "abcdefghijklmnopqrstuvwxyz", True)
-        self.order = self.calc_indices()
+        TransCipher.__init__(self, key.upper())
 
-    def calc_indices(self):
+    def get_encrypt_order(self):
         indices = []
         unsorted_letters = [(self.key[i], i) for i in range(len(self.key))]
         sorted_letters = [(letter, index) for letter, index in sorted(unsorted_letters)]
         return [pair[1] for pair in sorted_letters]
 
-
+    def get_decrypt_order(self):
+        return
     
+    def make_columns(self, text):
+        columns = []
+        keylen = len(self.key)
+        while len(self.text) % keylen != 0:
+            text += "X"
+        for col_num in range(keylen):
+            columns.append("")
+        for i in range(len(text)):
+            columns[i%keylen] += text[i]
+        return columns
+
+    def encrypt(self, text, keep_spaces = False, keep_punct = False):
+        text = self.prep_text(self, text, keep_spaces, keep_punct)
+        columns = self.make_columns(self, text)
